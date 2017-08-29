3b53e34e526c176a34949ab576ba38864179bf7d
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 22:38:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (var i = 0; i <= musicians.length; i++) {
-      array.push() = (`${musicians[i]} plays ${instruments[i]}`)
+      member = musicians[i]
+      instrument = instruments[i]
+      array.push() = (`${member} plays ${instrument}`)
     }
   }
   return array
